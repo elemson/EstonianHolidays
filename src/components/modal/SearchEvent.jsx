fac4b228aab476e6ee9915ba7e6848f3fe1cc476
elemson <toluwhani@gmail.com>
2020-05-23 02:43:32 +0300

    added bootstrap modal

---
+++ src/components/modal/SearchEvent.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SearchForm from "./SearchForm";
+
+const SearchEvent = () => {
+  return (
+    <>
+      <div>
+        <SearchForm />{" "}
+      </div>
+    </>
+  );
+};
+
+export default SearchEvent;
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,13 +1,21 @@
 import React from "react";
+import SearchEvent from "../modal/SearchEvent";
 
 export const Sidebar = () => {
   return (
     <div className="col-lg-3">
-      <button className="btn btn-primary btn-block">Create New Event</button>
+      <button
+        data-toggle="modal"
+        data-target="#search-event"
+        className="btn btn-primary btn-block"
+      >
+        Search New Event
+      </button>
 
       <div className="m-t-20">
         <br />
       </div>
+      <SearchEvent />
     </div>
   );
 };
